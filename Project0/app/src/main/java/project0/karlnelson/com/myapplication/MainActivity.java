cb2659e802622b946e833d29613479dc1bf3d37a
Karl <k.c.nelson7692@gmail.com>
2015-07-14 22:41:46 +1000

    Project 0 launch wireframe completed

--- Project0/app/src/main/java/project0/karlnelson/com/myapplication/MainActivity.java
+++ Project0/app/src/main/java/project0/karlnelson/com/myapplication/MainActivity.java
@@ -4,6 +4,9 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -12,6 +15,78 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Button streamerBtn = (Button) findViewById(R.id.btn_spotify_streamer);
+        streamerBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String text = getString(R.string.launch_message_start)
+                        + streamerBtn.getText().toString().toLowerCase()
+                        + getString(R.string.launch_message_end);
+                Toast.makeText(getApplicationContext(), text, Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+        final Button scoresBtn = (Button) findViewById(R.id.btn_scores_app);
+        scoresBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String text = getString(R.string.launch_message_start)
+                        + scoresBtn.getText().toString().toLowerCase()
+                        + getString(R.string.launch_message_end);
+                Toast.makeText(getApplicationContext(), text, Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+        final Button libraryBtn = (Button) findViewById(R.id.btn_library_app);
+        libraryBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String text = getString(R.string.launch_message_start)
+                        + libraryBtn.getText().toString().toLowerCase()
+                        + getString(R.string.launch_message_end);
+                Toast.makeText(getApplicationContext(), text, Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+        final Button buildItBiggerBtn = (Button) findViewById(R.id.btn_build_it_bigger);
+        buildItBiggerBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String text = getString(R.string.launch_message_start)
+                        + buildItBiggerBtn.getText().toString().toLowerCase()
+                        + getString(R.string.launch_message_end);
+                Toast.makeText(getApplicationContext(), text, Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+        final Button readerBtn = (Button) findViewById(R.id.btn_xyz_reader);
+        readerBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String text = getString(R.string.launch_message_start)
+                        + readerBtn.getText().toString().toLowerCase()
+                        + getString(R.string.launch_message_end);
+                Toast.makeText(getApplicationContext(), text, Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+        final Button capstoneBtn = (Button) findViewById(R.id.btn_capstone_my_app);
+        capstoneBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String text = getString(R.string.launch_message_start)
+                        + getString(R.string.btn_capstone_message)
+                        + getString(R.string.launch_message_end);
+                Toast.makeText(getApplicationContext(), text, Toast.LENGTH_SHORT).show();
+
+            }
+        });
     }
 
 
